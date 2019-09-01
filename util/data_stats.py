9fdb3b8b576ff5a2636e7ff1b7ab85fd40b6eec9
oziris <samo.sela@gmail.com>
2019-09-01 14:10:57 +0200

    Auxiliary function to compute datasets mean and std.

--- util/data_stats.py
+++ util/data_stats.py
@@ -1,21 +1,35 @@
 import os
 import torch
+from PIL import Image
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
 from torchvision.datasets import ImageFolder
-import torchvision.transforms as transforms
+from torchvision.transforms import ToTensor
+
+#PATH = '/media/samo/Workspace/ganomaly_pytorch/data/dl4cv'
+PATH = '/workspace/data/dl4cv'
 
 class MyDataset(Dataset):
     def __init__(self):
-        transform = transforms.Compose([transforms.ToTensor()])
-        self.dataset = ImageFolder(os.path.join('/media/samo/Workspace/ganomaly_pytorch/data/dl4cv', 'train'), transform)
+        self.data = ImageFolder(os.path.join(PATH, 'train'), loader=pil_loader_bw, transform=ToTensor())
         
     def __getitem__(self, index):
-        x = self.dataset[index]
-        return x
+        return self.data[index]
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self.data)
+
+def pil_loader_rgb(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+def pil_loader_bw(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('L')        
 
 def online_mean_and_std(loader):
     """Compute the mean and sd in an online fashion
@@ -26,13 +40,11 @@ def online_mean_and_std(loader):
     fst_moment = torch.empty(3)
     snd_moment = torch.empty(3)
 
-    for data in loader:
-        b, c, h, w = data.shape
-        print(data.shape)
-        exit
+    for x, y in loader: 
+        b, c, h, w = x.shape
         nb_pixels = b * h * w
-        sum_ = torch.sum(data, dim=[0, 2, 3])
-        sum_of_square = torch.sum(data ** 2, dim=[0, 2, 3])
+        sum_ = torch.sum(x, dim=[0, 2, 3])
+        sum_of_square = torch.sum(x ** 2, dim=[0, 2, 3])
         fst_moment = (cnt * fst_moment + sum_) / (cnt + nb_pixels)
         snd_moment = (cnt * snd_moment + sum_of_square) / (cnt + nb_pixels)
 
@@ -40,6 +52,26 @@ def online_mean_and_std(loader):
 
     return fst_moment, torch.sqrt(snd_moment - fst_moment ** 2)
   
+def online_mean_and_std_bw(loader):
+    """Compute the mean and sd in an online fashion
+
+        Var[x] = E[X^2] - E^2[X]
+    """
+    cnt = 0
+    fst_moment = torch.empty(1)
+    snd_moment = torch.empty(1)
+
+    for x, y in loader: 
+        b, c, h, w = x.shape
+        nb_pixels = b * h * w
+        sum_ = torch.sum(x, dim=[0, 2, 3])
+        sum_of_square = torch.sum(x ** 2, dim=[0, 2, 3])
+        fst_moment = (cnt * fst_moment + sum_) / (cnt + nb_pixels)
+        snd_moment = (cnt * snd_moment + sum_of_square) / (cnt + nb_pixels)
+
+        cnt += nb_pixels
+
+    return fst_moment, torch.sqrt(snd_moment - fst_moment ** 2)  
 
 dataset = MyDataset()
 loader = DataLoader(
@@ -49,5 +81,5 @@ loader = DataLoader(
     shuffle=False
 )
 
-mean, std = online_mean_and_std(loader)
+mean, std = online_mean_and_std_bw(loader)
 print("Mean {}, std {}".format(mean, std))
